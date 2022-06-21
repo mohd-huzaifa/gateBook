c455ff5af347bc2f520dad4cdb5087a0a0a7825d
Mohd Huzaifa <mohdhuzaifa8126195456@gmail.com>
2022-06-22 00:33:05 +0530

    All files have been uploaded

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',function(req,res){
+    res.sendFile('index.html');
+})
+
+var searchTopic;
+
+app.post('/home',function(req,res){
+   
+       searchTopic = req.body.topic + '.html';
+       res.redirect(303,'/search');
+     
+    
+})
+app.get('/search',function(req,res){
+    res.sendFile( __dirname+'/public/'+ searchTopic);
+})
+   
+
+
+app.listen(4500,function(){
+    console.log("Your server is running");
+})
